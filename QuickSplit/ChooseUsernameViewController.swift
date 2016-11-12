1fdf25043bd278ecb3ebfaf493f5360f862bb1ac
Rachit Kataria <rachitka@usc.edu>
2016-11-12 12:31:06 -0800

    Updated view controller for choosing usernames

--- QuickSplit/ChooseUsernameViewController.swift
+++ QuickSplit/ChooseUsernameViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ChooseUsernameViewController: UIViewController {
 
+    @IBOutlet weak var addUserButton: UIButton!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var chooseUsersLabel: UILabel!
+    
+    @IBOutlet weak var user1TextField: UITextField!
+    @IBOutlet weak var user2TextField: UITextField!
+    
+    @IBOutlet weak var user3TextField: UITextField!
+    @IBOutlet weak var user4TextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
 
@@ -21,8 +30,15 @@ class ChooseUsernameViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
+    @IBAction func addUserButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func doneButtonClicked(_ sender: Any) {
+    }
+    
     /*
+     
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
