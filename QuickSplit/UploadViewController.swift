38fab980d38cf30720d8ad0fcef291ca31e441e5
Kelly Lampotang <klampotang@gmail.com>
2016-11-12 12:11:04 -0800

    storyboard for upload

--- QuickSplit/UploadViewController.swift
+++ QuickSplit/UploadViewController.swift
@@ -10,12 +10,19 @@ import UIKit
 
 class UploadViewController: UIViewController {
 
+    @IBOutlet weak var splitButton: UIButton!
+    @IBOutlet weak var uploadButton: UIButton!
+    @IBOutlet weak var imageReceipt: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func splitClicked(_ sender: Any) {
+    }
+    @IBAction func uploadButtonClicked(_ sender: Any) {
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
