04749e8ab84420f9c243a7f501af8fdfee0d3443
Andrew Jiang <andrewji@usc.edu>
2016-11-13 01:58:00 -0800

    compress imgur

---
+++ QuickSplit/ImgurUpload.swift
@@ -0,0 +1,18 @@
+//
+//  ImgurUpload.swift
+//  QuickSplit
+//
+//  Created by Andrew Jiang on 11/13/16.
+//  Copyright © 2016 Rachit Kataria. All rights reserved.
+//
+
+import UIKit
+
+struct ImgurUpload {
+    
+    static let mult: Double = 1.0
+    static let width: Double = 333.0
+    static let height: Double = 444.0
+    
+    
+}
