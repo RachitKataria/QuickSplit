04738bb60ff98064a2501ec0d0ff2de6af42e1a4
Kelly Lampotang <klampotang@gmail.com>
2016-12-22 17:17:55 -0500

    uses zip codes now

--- QuickSplit/SettingsViewController.swift
+++ QuickSplit/SettingsViewController.swift
@@ -9,16 +9,14 @@
 import UIKit
 import CoreLocation
 
-class SettingsViewController: UIViewController, CLLocationManagerDelegate, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
-    @IBOutlet weak var searchBar: UISearchBar!
+class SettingsViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var currentCityLabel: UILabel!
-    @IBOutlet weak var searchTableView: UITableView!
     @IBOutlet weak var taxPercentageLabel: UILabel!
 
     let locationManager = CLLocationManager()
     let API_KEY = "AIzaSyDl7xbKCu609eOsFo4zMsfWGnU5Tk_45OY"
-    
+    var newWordField: UITextField?
     var cityTyped = ""
     var currLocation = ""
     var locationChosen = ""
@@ -26,11 +24,6 @@ class SettingsViewController: UIViewController, CLLocationManagerDelegate, UITab
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        
-        
-        searchTableView.dataSource = self
-        searchTableView.delegate = self
-        searchBar.delegate = self
         
         //LOAD FROM USER DEFAULTS
         
@@ -43,33 +36,14 @@ class SettingsViewController: UIViewController, CLLocationManagerDelegate, UITab
             currentCityLabel.text = city!
         }
         
-        if(segmentedControl.selectedSegmentIndex == 0) {
-            searchBar.isHidden = true
-            searchBar.isHidden = true
-        }
-        else {
-            searchTableView.isHidden = false
-            searchBar.isHidden = false
-        }
-        
-        
         //Location setup
-        
         locationManager.delegate = self //sets the class as delegate for locationManager
         locationManager.desiredAccuracy = kCLLocationAccuracyBest //specifies the location accuracy
         //locationManager.requestAlwaysAuthorization()
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation() //starts receiving location updates from CoreLocation
+    }
 
-    }
-    
-    func searchBar(_ searchBar: UISearchBar, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-        searchTableView.isHidden = false
-        let newText = NSString(string: searchBar.text!).replacingCharacters(in: range, with: text)
-        cityTyped = newText
-        fetchCities(query: cityTyped)
-        return true
-    }
 
     func fetchCities(query: String) {
         print("google apps api being called")
@@ -90,10 +64,10 @@ class SettingsViewController: UIViewController, CLLocationManagerDelegate, UITab
                 if let responseDictionary = try! JSONSerialization.jsonObject(with: data, options:[]) as? NSDictionary {
                     self.results = responseDictionary.value(forKeyPath: "predictions") as! NSArray
                     //print(self.results)
-                    self.searchTableView.reloadData()
                 }
             }
         });
+        
         task.resume()
     }
     override func didReceiveMemoryWarning() {
@@ -101,29 +75,32 @@ class SettingsViewController: UIViewController, CLLocationManagerDelegate, UITab
         // Dispose of any resources that can be recreated.
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "SearchCell") as! SearchTableViewCell
-        cell.backgroundColor = UIColor.clear
-        cell.searchResultLabel.text = (((results[indexPath.row]) as! NSDictionary).value(forKey: "description") as? String)!
-        return cell
-    }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return results.count 
-    }
     
-
+    func addTextField(textField: UITextField!){
+        // add the text field and make the result global
+        textField.placeholder = "Definition"
+        self.newWordField = textField
+    }
+    func zipEntered(alert: UIAlertAction!){
+        // store the new word
+        self.currentCityLabel.text = self.newWordField?.text
+        
+        let defaults = UserDefaults.standard
+        defaults.set(self.newWordField?.text, forKey: "city")
+        defaults.synchronize()
+    }
     @IBAction func segControlValueChanged(_ sender: Any) {
         if(segmentedControl.selectedSegmentIndex == 0) { //Current city
-            searchTableView.isHidden = true
-            searchBar.isHidden = true
             //Current City label should display current city
             currentCityLabel.text = currLocation
             
         }
         else { //Search for a city
-            currentCityLabel.text = ""
-            searchTableView.isHidden = false
-            searchBar.isHidden = false
+            let newWordPrompt = UIAlertController(title: "Enter zip code", message: "Zip Code", preferredStyle: UIAlertControllerStyle.alert)
+            newWordPrompt.addTextField(configurationHandler: addTextField)
+            newWordPrompt.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.default, handler: nil))
+            newWordPrompt.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: zipEntered))
+            present(newWordPrompt, animated: true, completion: nil)
         }
         
         let defaults = UserDefaults.standard
@@ -162,9 +139,9 @@ class SettingsViewController: UIViewController, CLLocationManagerDelegate, UITab
         if let containsPlacemark = placemark {
             //stop updating location to save battery life
             locationManager.stopUpdatingLocation()
-            
+            print(containsPlacemark)
             //Pass into global currlat and long variables
-            currLocation = ("\((containsPlacemark.locality)!), \((containsPlacemark.administrativeArea)!)")
+            currLocation = ("\((containsPlacemark.postalCode)!), \((containsPlacemark.country)!)")
             print(currLocation)
             currentCityLabel.text = currLocation
             let defaults = UserDefaults.standard
@@ -174,15 +151,8 @@ class SettingsViewController: UIViewController, CLLocationManagerDelegate, UITab
         
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        locationChosen = (((results[indexPath.row]) as! NSDictionary).value(forKey: "description") as? String)!
-        print(locationChosen)
-        currentCityLabel.text = locationChosen
-        let defaults = UserDefaults.standard
-        defaults.set(locationChosen, forKey: "city")
-        defaults.synchronize()
-        searchTableView.isHidden = true
-    }
+    
+    
    
 
 }
